b10ea9156a3e532c6cf2ca0127aabe291282ea49
rahul <rahulmgrahul2004@gmail.com>
2025-11-26 11:03:09 +0530

    M5  C2

---
+++ Module 5 - Exception/Chapter 2/student/Rahul/exercise/exercise_2/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:exercise_2/page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Abcd(),
+    )
+  );
+}
---
+++ Module 5 - Exception/Chapter 2/student/Rahul/exercise/student_reg_1/test/student_reg_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:student_reg_1/student_reg_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
